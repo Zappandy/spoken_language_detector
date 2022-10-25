ec0c3f2bb9618ff840f456327ebd15b54fb8723e
Andres <nanoandres_24@hotmail.com>
2022-10-25 11:40:57 +0200

    more preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,7 +5,8 @@ from torch.utils.data import DataLoader, random_split, Subset
 work_dir = "/media/andres/"  # complete path later and folders
 
 train_data = # from dir
-train_data = Subset(train_data, torch.arange(240))
-test_data = Subset(test_data, torch.arange(60))
+train_data = Subset(train_data, torch.arange(240)) # 80% 
+test_data = Subset(test_data, torch.arange(60))  # 20%
 
 train_dataloader = DataLoader(train_data, batch_size=16, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=16, shuffle=True)
