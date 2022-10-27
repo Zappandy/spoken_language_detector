3ffb4d3a019fe682436d9d766991a3b281b8fa61
Andres <nanoandres_24@hotmail.com>
2022-10-27 21:56:09 +0200

    adding sample of data loading

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,10 +1,30 @@
 import torch
 import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader, random_split, Subset
+from torch.utils.data import DataLoader, Dataset, random_split, Subset
 
-work_dir = "/media/andres/"  # complete path later and folders
+# ls /media/andres/2D2DA2454B8413B5/test/test/ | grep -o '.....$' | uniq   
+train_dir = "/media/andres/2D2DA2454B8413B5/train/train/"
+test_dir = "/media/andres/2D2DA2454B8413B5/test/test/"
 
-train_data = # from dir
+class SpeechDataset(Dataset):
+
+    def __init__(self):
+        numbers = [i for i in range(100)]
+        self.data = numbers
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+train_data = SpeechDataset()
+dataloader = DataLoader(train_data, batch_size=10, shuffle=True)
+
+for i, batch in enumerate(dataloader):
+    print(i, batch)
+
+raise SystemExit
 train_data = Subset(train_data, torch.arange(240)) # 80% 
 test_data = Subset(test_data, torch.arange(60))  # 20%
 
