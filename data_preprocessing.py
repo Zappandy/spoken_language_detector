626a86a57cc94379a7a00fe7ad316df26357c979
Andres <nanoandres_24@hotmail.com>
2022-10-30 18:38:21 +0100

    specified wheel install pkg

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -13,10 +13,21 @@ base_path = "/media/andres/2D2DA2454B8413B5/"
 train_dir = base_path + "train/train/"
 test_dir = base_path + "test/test/"
 
+def flac_to_spectro(file_path):
+    waveform, sample_rate = torchaudio.load(file_path, normalize=True)
+    transform = T.MelSpectrogram(sample_rate)
+    mel_specgram = transform(waveform)
+    return mel_specgram  
+
 class SpeechDataset(Dataset):
 
     def __init__(self, flac_dir):
         self.audio_path_list = sorted(self.find_files(flac_dir))
+        
+        # self.specto_data = []
+        # for f_path in self.audio_path_list:
+        #     self.specto_data.append(flac_to_spectro(f_path))
+        
 
     def __len__(self):
         return len(self.audio_path_list)
@@ -26,6 +37,7 @@ class SpeechDataset(Dataset):
         with open(audio_path, "rb") as f:
             data, samplerate = sf.read(f)
         return data, samplerate
+        #return self.specto_data[index]  if using mathilde's method
 
     def find_files(self, directory, pattern="*.flac"):
         """
