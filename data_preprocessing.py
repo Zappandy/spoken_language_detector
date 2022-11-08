fc76e007877d4399204f5d1ef5af1572b0b7c71a
Andres <nanoandres_24@hotmail.com>
2022-11-08 14:57:33 +0100

    adding sources and new data_preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,7 +1,11 @@
 import torch
+import torchaudio
+import torchaudio.transforms as T
+import numpy as np
 import fnmatch
 import matplotlib.pyplot as plt
-import soundfile as sf
+import soundfile as sf  # maybe unneeded if all loading performed with librosa!
+import librosa
 import os
 from torch.utils.data import DataLoader, Dataset, random_split, Subset
 
@@ -9,57 +13,78 @@ from torch.utils.data import DataLoader, Dataset, random_split, Subset
 # used to clean up es files
 # ls /media/andres/2D2DA2454B8413B5/test/test/ | grep -o '.....$' | uniq   # exploring file types
 #/media/andres/2D2DA2454B8413B5/test/test/ | grep -o '^es.*'
+# https://gist.github.com/mthrok/01f89d9bc27a7fe618bf5e8ef71b44ba
+# librosa vs torchaudio spectograms
+# worth using sox to check sampling rate https://stackoverflow.com/questions/4497419/how-do-i-get-an-audio-file-sample-rate-using-sox
 base_path = "/media/andres/2D2DA2454B8413B5/"
 train_dir = base_path + "train/train/"
 test_dir = base_path + "test/test/"
 
-def flac_to_spectro(file_path):
-    waveform, sample_rate = torchaudio.load(file_path, normalize=True)
-    transform = T.MelSpectrogram(sample_rate)
-    mel_specgram = transform(waveform)
-    return mel_specgram  
 
 class SpeechDataset(Dataset):
 
-    def __init__(self, flac_dir):
-        self.audio_path_list = sorted(self.find_files(flac_dir))
+    def __init__(self, flac_dir, load_method):
+        self.audio_path_list = sorted(self.find_files(flac_dir))  # do we need them to be sorted?
+        methods = {"librosa": self.flac2melspec, "soundfile": self.sf_loader, "torchaudio": self.flac_to_spectro}
+        chosen_method = methods[load_method]
+        self.specto_data = [chosen_method(audio) for audio in self.audio_path_list]
         
-        # self.specto_data = []
-        # for f_path in self.audio_path_list:
-        #     self.specto_data.append(flac_to_spectro(f_path))
         
-
     def __len__(self):
-        return len(self.audio_path_list)
+        return len(self.specto_data)
 
     def __getitem__(self, index):
-        audio_path = self.audio_path_list[index]
-        with open(audio_path, "rb") as f:
+        return self.specto_data[index]  
+
+    def find_files(self, directory, pattern=".flac"):
+        """
+        Recursive search method to find files. Credit to Paul Magron and Andrea de Marco
+        for OG method
+        """
+
+        return  [f.path for f in os.scandir(test_dir) if f.path.endswith(pattern)]  # ends with does not like regex
+
+    def flac_to_spectro(self, file_path):
+        waveform, sample_rate = torchaudio.load(file_path, normalize=True)
+        transform = T.MelSpectrogram(sample_rate)        
+        return transform(waveform), sample_rate
+        
+    def flac2melspec(self, file_path, n_mels=64, melspec_size=512):
+        sig, fs =  librosa.load(file_path, sr=None)
+        sig /= np.max(np.abs(sig), axis=0)
+        n_fft = melspec_size
+        hop_length = int(n_fft/2)
+
+        # padding signal if less than a second
+        if len(sig) < fs:
+            padded_array = np.zeros(fs)
+            padded_array[:np.shape(sig)[0]] = sig
+            sig = padded_array
+            print("padding!!!")
+
+        melspec = librosa.feature.melspectrogram(y=sig, sr=fs,
+                                                 center=True, n_fft=n_fft,
+                                                 hop_length=hop_length, n_mels=n_mels)
+
+        # TODO:plotting?
+
+        melspec = librosa.power_to_db(melspec, ref=1.0)
+        melspec /= 80.0  # highest db...
+        return melspec, fs
+        #TODO: check melspec?
+
+
+    def sf_loader(self, file_path):
+        with open(file_path, "rb") as f:
             data, samplerate = sf.read(f)
         return data, samplerate
-        #return self.specto_data[index]  if using mathilde's method
 
-    def find_files(self, directory, pattern="*.flac"):
-        """
-        Recursive search method to find files. Credit to Paul Magron for OG method
-        """
-        audio_path_list = []
-        for root, dirnames, filenames in os.walk(directory):
-            for filename in fnmatch.filter(filenames, pattern):
-                audio_path_list.append(os.path.join(root, filename))
-
-        return audio_path_list
-
-test_data = SpeechDataset(test_dir)  # 540
+test_data = SpeechDataset(test_dir, "librosa")  # 540
 test_dataloader = DataLoader(test_data, batch_size=4, shuffle=True)
 
-for data, samplerate in test_dataloader:
-    print(data, samplerate)
-    print()
-    print(data.shape, samplerate.shape)
-    break
-
 raise SystemExit
+
+#TODO  finish up the splits
 train_data = Subset(train_data, torch.arange(240)) # 80% 
 test_data = Subset(test_data, torch.arange(60))  # 20%
 
