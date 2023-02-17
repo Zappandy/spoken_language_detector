53dd8a94ba0bf724cff1daf661519f0047dda9e5
MathildeAguiar <60926855+MathildeAguiar@users.noreply.github.com>
2023-02-17 17:42:29 +0100

    Rm usuless comments

--- experiment_runner.py
+++ experiment_runner.py
@@ -1,24 +1,16 @@
-# example, tst data has 540 files
 import torch
 from dataloader import SpeechDataset, get_balanced_subset
 from model import CNNSpeechClassifier
 from train import MyTrainer
 from torch.utils.data import DataLoader, Subset, random_split
 
-#torch.manual_seed(0)
 SEED = 42
 train_dir = "./Dataset/train/train"
 
 train_data = SpeechDataset(train_dir, "librosa")
 
-
-
-#train_data = get_balanced_subset(train_data, 1000, 'f') #4000 files for each language
-
-
 train_size = int(len(train_data) * 0.8)
 val_size = len(train_data) - train_size
-
 
 train_data, val_data = random_split(train_data, [train_size, val_size], generator=torch.Generator().manual_seed(SEED))
 train_dataloader = DataLoader(train_data, batch_size=8, shuffle=True)  # 8, 64, 862 - 8
